063699e5742e22976efd52ddeb4406ab40203d12
Aleksandra Janda <aleksandra_janda@wp.pl>
2018-11-04 10:22:00 +0100

    Update src/BubbleSortZadania.java

--- src/BubbleSortZadania.java
+++ src/BubbleSortZadania.java
@@ -1,31 +1,42 @@
 public class BubbleSortZadania {
-    public static int[] zadanie1(int[] array, int e1, int e2){
+    public static void zadanie1(int[] array, int e1, int e2){
         int temp = array[e1];
         array[e1]=array[e2];
         array[e2]=temp;
-        return array;
     }
     public static void zadanie2(int[] array){
-        for(int x:array){
-            System.out.print(x+"\t");
+        for(int x=0; x<array.length;x++){
+            System.out.print(x+":"+array[x]+"\t");
         }
-        //System.out.println();
     }
     public static void zadanie3(int[] array){
         int i = 0;
         while(i<array.length){
             zadanie2(array);
+            System.out.println();
+            i++;
         }
         System.out.println();
     }
+    public static void zadanie4(int[] array){
+        for(int i=0;i<array.length;i++){
+            if(i-1>=0) {
+                zadanie1(array, i-1, i);
+            }
+            zadanie2(array);
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        int[] array = new int[]{10,24,36,42};
+        int[] array = new int[]{10,20,30,40};
         int e1 = 0;
         int e2 = 3;
-        int[] array2 = zadanie1(array,e1,e2);
-        zadanie2(array2);
-        System.out.println();
-        zadanie3(array2);
+        //int[] array2 = zadanie1(array,e1,e2);
+        //zadanie2(array2);
+        //System.out.println();
+        zadanie3(array);
+        //System.out.println();
+        zadanie4(array);
     }
 }
